c64e646f190bff95a11face7d2b7e52c8db2413e
netdol365 <netdol365@naver.com>
2022-03-03 14:24:43 +0900

    first commit

---
+++ new-viewer/components/NewsList.js
@@ -0,0 +1,33 @@
+import Newsitem from "./NewsItem";
+
+const NewsListBlock = styled.div`
+    box-sizing : border-box;
+    padding-bottom : 3rem;
+    width : 768px;
+    margin 0 auto;
+    margin-top: : 2rem;
+    @media screen and (max-width : 78px){
+        width : 100%;
+        padding-left : 1rem;
+        padding-right : 1rem;
+    }
+`;
+
+const sampleArticle={
+    title : 'TITLE',
+    description : 'NewsLetters...',
+    url : 'https://google.com',
+    urlToImage : 'https://via.placeholder.com/160',         // 임시 이미지, 제목, 내용 설정
+}
+
+export default function NewsList(){
+    return(
+        <NewsListBlock>
+            <Newsitem article={sampleArticle}></Newsitem>
+            <Newsitem article={sampleArticle}></Newsitem>
+            <Newsitem article={sampleArticle}></Newsitem>
+            <Newsitem article={sampleArticle}></Newsitem>
+            <Newsitem article={sampleArticle}></Newsitem>
+        </NewsListBlock>
+    );
+};
