690c8284d5dae69cb52d98f1cc61c66a1189787f
netdol365 <netdol365@naver.com>
2022-03-03 14:40:49 +0900

    commit on NewList

--- new-viewer/src/App.js
+++ new-viewer/src/App.js
@@ -1,28 +1,31 @@
 import './App.css';
 import axios from 'axios';
 import {useState} from 'react';
+import NewsList from './components/NewsList';
 
 function App() {
-  const [data,setData] = useState();
+  // const [data,setData] = useState();
 
-  const onClick = async ()=>{
-    try{
-      const response = await axios.get(
-        'https://newsapi.org/v2/top-headlines?country=kr&apiKey=1a76bbed90e44526bc279d6fc0048d4e',);setData(response.data);
+  // const onClick = async ()=>{
+  //   try{
+  //     const response = await axios.get(
+  //       'https://newsapi.org/v2/top-headlines?country=kr&apiKey=1a76bbed90e44526bc279d6fc0048d4e',);setData(response.data);
          
-      }catch(e){
-        console.log('error... occured');
-      }
-  };
+  //     }catch(e){
+  //       console.log('error... occured');
+  //     }
+  // };
 
-  return (
-    <div className="App">
-      <div>
-        <button id="load" onClick={onClick}>Load</button>
-      </div>
-      {data && <textarea rows={7} value={JSON.stringify(data,null,2)} readOnly = {true}></textarea>}  
-    </div>
-  );
+  // return (
+  //   <div className="App">
+  //     <div>
+  //       <button id="load" onClick={onClick}>Load</button>
+  //     </div>
+  //     {data && <textarea rows={7} value={JSON.stringify(data,null,2)} readOnly = {true}></textarea>}  
+  //   </div>
+  // );
+
+  return <NewsList />;
 }
 
 export default App;
