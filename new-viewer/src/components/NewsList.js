29ab1446c66493101011fbc65f910b23b50b8378
netdol365 <netdol365@naver.com>
2022-03-03 15:24:46 +0900

    add useState & useEffect

--- new-viewer/src/components/NewsList.js
+++ new-viewer/src/components/NewsList.js
@@ -1,5 +1,7 @@
 import Newsitem from "./NewsItem";
 import styled from "styled-components";
+import axios from "axios";
+import { useState, useEffect } from "react";
 
 const NewsListBlock = styled.div`
     box-sizing : border-box;
@@ -14,22 +16,42 @@ const NewsListBlock = styled.div`
     }
 `;
 
-const sampleArticle={
-    title : 'TITLE',
-    description : 'NewsLetters...',
-    url : 'https://google.com',
-    urlToImage : 'https://via.placeholder.com/160',         // 임시 이미지, 제목, 내용 설정
-}
-
 export default function NewsList(){
+    const [articles,setArticles] = useState();
+    const [loading,setLoading] = useState(false);
+
+    useEffect(()=>{
+        const fetchData = async () =>{
+            setLoading(true);
+
+        try{
+            const response = await axios.get(
+                'https://newsapi.org/v2/top-headlines?country=kr&apiKey=1a76bbed90e44526bc279d6fc0048d4e',);
+                setArticles(response.data.articles);
+            }catch(e){
+                console.log(e.message);
+            }
+            setLoading(false);
+        };
+        fetchData();
+    },[]);
+
+    if(loading)                     // 로딩되는 중간에 사용자에게 가시화
+    {
+        return <NewsListBlock>Loading....</NewsListBlock>
+    }
+
+    if(!articles)                   // 기사값이 미설정시 반환
+    {
+        return null;
+    }
+
     return(
-        <NewsListBlock>
-            <Newsitem article={sampleArticle}></Newsitem>
-            <Newsitem article={sampleArticle}></Newsitem>
-            <Newsitem article={sampleArticle}></Newsitem>
-            <Newsitem article={sampleArticle}></Newsitem>
-            <Newsitem article={sampleArticle}></Newsitem>
-        </NewsListBlock>
-        
+       <NewsListBlock>
+           {articles.map(articles=>(
+               <Newsitem key={articles.url} article={articles}></Newsitem>
+           ))}
+           <hr></hr>
+       </NewsListBlock>
     );
 };
